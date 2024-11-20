aaf953117f1a88395f8172b677c6c94116af2bd2
unknown <you@example.com>
2024-11-20 16:12:36 +0200

    first commit

---
+++ js-basics.js
@@ -0,0 +1,56 @@
+
+//let firstName = "Mareks";
+
+//console.log(firstName);
+
+//let myArray = [1, 2, 3, 4];
+
+//let x = 10;
+//if(x == 10){
+//    console.log("x ir lielāks par 10");
+//}
+
+//let person = {
+  //  vards: "Antra",
+  //  irStudents: true,
+//}
+//console.log(person.irStudents) //dot notation
+//console.log(person["vards"]) //bracket notation
+
+//let attribute = "age"
+//console.log(person[attribute]);
+
+//console.log(person);
+//person.vecums = 16;
+//console.log(person);
+
+//let ageStatus = (person.vecums >= 18) ? "Pilngadīgs" : "Nepilngadīgs";
+//console.log(ageStatus);
+
+//let namesArray = ["Antra", "Ilmārs", "Pauls", "Renārs"] // indeksi ir attiecīgi [0, 1, 2, 3]
+
+//namesArray[2];
+
+//for(let i = 0; i < namesArray.length; i++){
+//    console.log(namesArray[i])
+//}
+//function with return value or without
+function drive(){
+    console.log("driving");
+}
+
+function getReactArea() {
+    return 3 * 4;
+  }
+ let result = getReactArea();
+  console.log(result)
+
+//function with parametrs
+//function call with arguments
+  function getAreaAdvanced(width, height) {
+    return width * height;
+  }
+ console.log (getAreaAdvanced(5, 6)) ;
+  
+
+
